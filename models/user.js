1ddad029cb7050ebede78002c9899fa0afaf0628
Jabar Saleem <jabzahmy@gmail.com>
2020-09-10 16:23:51 +0500

    https implementation with node.js

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var PassportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    firstname: {
+      type: String,
+        default: ''
+    },
+    lastname: {
+      type: String,
+        default: ''
+    },
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+});
+
+User.plugin(PassportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
